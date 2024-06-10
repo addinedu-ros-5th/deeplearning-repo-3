05db77268afefad261c6b581540f7982bca7f75f
YeongsuSon <163129674+ysu0415@users.noreply.github.com>
2024-06-10 12:24:11 +0900

    feat : GUI : video play

--- src/Total Test/Driving_Service(Client).py
+++ src/Total Test/Driving_Service(Client).py
@@ -12,13 +12,12 @@ import requests
 import time
 
 
-from_class = uic.loadUiType('login.ui')[0] # 현복님 login ui파일 
-sign_up_class = uic.loadUiType('signup.ui')[0] # 현복님 Sign_up ui 파일
+from_class = uic.loadUiType('login.ui')[0] 
+sign_up_class = uic.loadUiType('signup.ui')[0] 
 
-total_gui_class = uic.loadUiType('Window.ui')[0] # 영수님 Main_ui파일
-video_class = uic.loadUiType('video.ui')[0] # 영수님 video_ui파일
+total_gui_class = uic.loadUiType('Window.ui')[0] 
+video_class = uic.loadUiType('video.ui')[0] 
 
-# 실시간 데이터 전송 업데이트 Class
 class FileUploadThread(QThread):
     update_progress = pyqtSignal(int)
     finished = pyqtSignal(list)
@@ -40,33 +39,33 @@ class FileUploadThread(QThread):
                 if response.status_code == 201:
                     response_files.extend(response.json().get('processed_videos', []))
                 else:
-                    self.error.emit(f"파일 전송 실패: {response.status_code}")
+                    self.error.emit(f"File transfer failed: {response.status_code}")
                     return
 
                 progress = (i + 1) / total_files * 100
-                self.update_progress.emit(int(progress))  # 신호를 발생시킵니다.
+                self.update_progress.emit(int(progress))
 
         except Exception as e:
-            self.error.emit(f"파일 전송 중 오류 발생: {str(e)}")
+            self.error.emit(f"Error occurred while transferring file: {str(e)}")
             return
 
         self.finished.emit(response_files)
         
 class VideoPlayer(QMainWindow, video_class):
-    def __init__(self):
+    def __init__(self, video_path=None):
         super(VideoPlayer, self).__init__()
         self.setupUi(self)
         
         self.setWindowTitle("Visual Resualt") 
         
-        self.video_path = '40km-h_.mp4'
+        self.video_path = video_path
         self.cap = cv2.VideoCapture(self.video_path)
         self.cap = None
         self.timer = QTimer()
         self.timer.timeout.connect(self.next_frame)
         self.frame_position = 0
         self.playing = False
-        self.memo_file_name = None  # 메모 파일 경로
+        self.memo_file_name = None 
 
         self.btn_play.clicked.connect(self.play_pause)
         self.btn_stop.clicked.connect(self.stop)
@@ -115,10 +114,8 @@ class VideoPlayer(QMainWindow, video_class):
             self.frame_position += 1
             self.bar.setValue(self.frame_position)
             
-            # Convert the frame to RGB format
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             
-            # Resize the frame while keeping the aspect ratio
             h, w, ch = frame.shape
             label_height, label_width = self.view.height(), self.view.width()
             aspect_ratio = w / h
@@ -131,12 +128,10 @@ class VideoPlayer(QMainWindow, video_class):
             
             resized_frame = cv2.resize(frame, (new_width, new_height), interpolation=cv2.INTER_AREA)
             
-            # Create a QImage and set it to the QLabel
             bytes_per_line = ch * new_width
             qt_image = QImage(resized_frame.data, new_width, new_height, bytes_per_line, QImage.Format_RGB888)
             self.view.setPixmap(QPixmap.fromImage(qt_image).scaled(self.view.size(), Qt.KeepAspectRatio))
 
-            # Update time label
             total_frames = self.cap.get(cv2.CAP_PROP_FRAME_COUNT)
             fps = self.cap.get(cv2.CAP_PROP_FPS)
             current_time = QTime(0, 0).addSecs(int(self.frame_position / fps))
@@ -162,9 +157,8 @@ class VideoPlayer(QMainWindow, video_class):
                 memo_text = self.memotext.toPlainText()
                 with open(file_name, 'w') as file:
                     file.write(memo_text)
-                self.memo_file_name = file_name  # 새로 저장된 파일 경로 저장
+                self.memo_file_name = file_name  
 
-# Total GUI 화면 클래스 (영수님 mainwindow)
 class Total_gui_Window(QMainWindow, total_gui_class):
     def __init__(self):
         super().__init__()
@@ -172,14 +166,12 @@ class Total_gui_Window(QMainWindow, total_gui_class):
         
         self.setWindowTitle("Main Window") 
         
-        # 메뉴용 버튼
         self.windows.clicked.connect(self.switch_to_windowPage)
         self.windows1.clicked.connect(self.switch_to_windowPage)
-        self.sign1.clicked.connect(self.switch_login) # Logout 버튼
-        self.sign.clicked.connect(self.switch_login) # Logout 버튼
+        self.sign1.clicked.connect(self.switch_login)
+        self.sign.clicked.connect(self.switch_login) 
         self.icon_widget.setHidden(True)
         
-        # 윈도우창 내 버튼용
         self.upload.clicked.connect(self.upload_files)
         self.analyze.clicked.connect(self.analyze_files)
         self.delbtn.clicked.connect(self.delete_selected_rows)
@@ -187,56 +179,52 @@ class Total_gui_Window(QMainWindow, total_gui_class):
         self.searchbar.setPlaceholderText("Enter text to search")
 
         self.tableWidget.setColumnCount(4)
-        self.tableWidget.setHorizontalHeaderLabels(['선택', '파일경로', '파일 이름', '상태'])
-        self.tableWidget.cellClicked.connect(self.toggle_checkbox)
+        self.tableWidget.setHorizontalHeaderLabels(['Check', 'File Path', 'File Name', 'Status'])
+        self.tableWidget.cellClicked.connect(self.toggle_checkbox) 
         self.tableWidget.horizontalHeader().setSectionResizeMode(2, QHeaderView.Stretch)
         header = self.tableWidget.horizontalHeader()
         header.setSectionResizeMode(0, QHeaderView.ResizeToContents)
-        self.tableWidget.setColumnHidden(1, True)  # 파일 경로 열 숨기기
+        self.tableWidget.setColumnHidden(1, True) 
         
         self.tableWidget.cellClicked.connect(self.cell_was_clicked)
         
         self.tableWidget1.setColumnCount(6)
-        self.tableWidget1.setHorizontalHeaderLabels(['파일경로', 'Video ID', 'Speed', 'Pedestrian', 'Traffic', 'Fail Num'])
+        self.tableWidget1.setHorizontalHeaderLabels(['File path', 'Video ID', 'Speed', 'Pedestrian', 'Traffic', 'Fail Num'])
         self.tableWidget1.cellClicked.connect(self.cell_was_clicked)
         self.tableWidget1.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.tableWidget1.setColumnHidden(0, True)  # 파일 경로 열 숨기기
+        self.tableWidget1.setColumnHidden(0, True)  
         
-    #메뉴 배너용 창 전환 설정
     def switch_to_windowPage(self):
         self.stackedWidget.setCurrentIndex(0)
     
-    #메인창 기능 구현
     def upload_files(self):
         options = QFileDialog.Options()
         files, _ = QFileDialog.getOpenFileNames(self, "파일 선택", "", "비디오 파일 (*.mp4 *.avi *.mov *.mkv);;모든 파일 (*)", options=options)
         if files:
             for file in files:
-                file_name = os.path.basename(file)  # 파일 이름 추출
+                file_name = os.path.basename(file)  
                 file_path = file
-                # 이미 존재하는 파일인지 확인
                 for row in range(self.tableWidget.rowCount()):
                     existing_file_name = self.tableWidget.item(row, 2).text()
                     if file_name == existing_file_name:
-                        reply = QMessageBox.question(self, '경고', f"'{file_name}' 파일은 이미 존재합니다.", 
+                        reply = QMessageBox.question(self, 'Warning', f"'{file_name}' The file is already exiests.", 
                                                      QMessageBox.Yes)
                         if reply == QMessageBox.Yes:
-                            self.upload_files()  # 파일 탐색기 대화 상자 다시 열기
+                            self.upload_files() 
                         return
-                else:  # 이미 존재하지 않는 파일인 경우에만 테이블 위젯에 추가
+                else:  
                     row_position = self.tableWidget.rowCount()
                     self.tableWidget.insertRow(row_position)
                     self.tableWidget.setItem(row_position, 2, QTableWidgetItem(file_name))
-                    self.tableWidget.setItem(row_position, 1, QTableWidgetItem(file_path))  # 파일 경로 추가
+                    self.tableWidget.setItem(row_position, 1, QTableWidgetItem(file_path)) 
                 
-                # 체크 박스 설정
                 checkbox_widget = QWidget()
                 checkbox = QCheckBox()
-                checkbox.setEnabled(False)  # 사용자가 선택할 수 없도록 설정
+                checkbox.setEnabled(False)
                 checkbox.setChecked(False)
                 checkbox_layout = QHBoxLayout(checkbox_widget)
                 checkbox_layout.addWidget(checkbox)
-                checkbox_layout.setAlignment(Qt.AlignCenter)  # 가운데 정렬 설정
+                checkbox_layout.setAlignment(Qt.AlignCenter)  
                 checkbox_layout.setContentsMargins(0, 0, 0, 0)
                 self.tableWidget.setCellWidget(row_position, 0, checkbox_widget)
     
@@ -247,11 +235,11 @@ class Total_gui_Window(QMainWindow, total_gui_class):
             checkbox = checkbox_widget.findChild(QCheckBox)
             if checkbox.isChecked():
                 rows_to_delete.append(row)
-        for row in reversed(rows_to_delete):  # 역순으로 삭제하여 인덱스 문제 방지
+        for row in reversed(rows_to_delete): 
             self.tableWidget.removeRow(row)
                 
     def toggle_checkbox(self, row, column):
-        if column == 2:  # 파일 경로 열 클릭 시 체크박스 토글
+        if column == 2:  
             checkbox_widget = self.tableWidget.cellWidget(row, 0)
             checkbox = checkbox_widget.findChild(QCheckBox)
             checkbox.setChecked(not checkbox.isChecked())
@@ -272,7 +260,7 @@ class Total_gui_Window(QMainWindow, total_gui_class):
     def analyze_files(self):
         row_count = self.tableWidget.rowCount()
         if row_count == 0:
-            QMessageBox.warning(self, "경고", "분석할 파일이 없습니다.")
+            QMessageBox.warning(self, "Warning", "There are no files to analyze.")
             return
 
         files_to_analyze = []
@@ -280,7 +268,6 @@ class Total_gui_Window(QMainWindow, total_gui_class):
             file_path = self.tableWidget.item(row, 1).text()
             files_to_analyze.append(file_path)
 
-        # 진행 바 창 설정
         self.progress_dialog = QProgressDialog("Analyzing files...", "Cancel", 0, len(files_to_analyze), self)
         self.progress_dialog.setWindowModality(Qt.WindowModal)
         self.progress_dialog.setValue(0)
@@ -288,7 +275,6 @@ class Total_gui_Window(QMainWindow, total_gui_class):
         url = 'http://192.168.0.126:5000/api/upload'
         self.thread = FileUploadThread(files_to_analyze, url)
         self.thread.update_progress.connect(self.update_progress)
-        #self.thread.finished.connect(self.on_finished)
         self.thread.error.connect(self.on_error)
         self.thread.start()
         self.progress_dialog.canceled.connect(self.cancel_analysis)
@@ -297,7 +283,7 @@ class Total_gui_Window(QMainWindow, total_gui_class):
         self.progress_dialog.setValue(value)
         if value == self.tableWidget.rowCount():
             self.progress_dialog.close()
-            QMessageBox.information(self, "완료", "모든 파일 분석이 완료되었습니다.")
+            QMessageBox.information(self, "Successed", "All files have been analyzed.")
 
     def on_error(self, error_message):
         self.progress_dialog.close()
@@ -305,43 +291,44 @@ class Total_gui_Window(QMainWindow, total_gui_class):
 
     def cancel_analysis(self):
         self.thread.terminate()
-        self.progress_dialog.close()  # 진행 바 창 닫기
+        self.progress_dialog.close() 
 
     def update_table_status(self, value, file_name, status):
-        for row in range(self.tableWidget.rowCount()):
-            if self.tableWidget.item(row, 2).text() == file_name:
-                self.tableWidget.setItem(row, 3, QTableWidgetItem(status))
+        for row in range(self.tableWidget1.rowCount()):
+            if self.tableWidget1.item(row, 1).text() == file_name:
+                self.tableWidget1.setItem(row, 5, QTableWidgetItem(status))
                 break
         self.update_progress(value)
     
-    #분석 테이블에서 특정 셀 클릭 시 비디오창이 열리게 하는 코드    
     def cell_was_clicked(self, row, column):
-        # 3번째 열 (index 2) 클릭 시 다른 UI 파일 열기
-        if column == 3:
-            self.open_video_ui()
+        if column == 5:
+            video_file_name = self.tableWidget1.item(row, 1).text()
+            self.open_video_ui(video_file_name)
             
-    def open_video_ui(self):
-        self.other_window = video.VideoPlayer()
+    def open_video_ui(self, video_file_name):
+        self.other_window = VideoPlayer(video_file_name)
         self.other_window.show()
 
-        # Total gui 화면 창 전환 (로그인 완료)
     def open_total_gui_window(self):
         self.total_gui_window = Total_gui_Window()
         self.total_gui_window.show()
         self.close()
 
-    # Logout 기능
     def switch_login(self):
-        reply = QMessageBox.question(self, 'Message', '로그인 화면으로 돌아가시겠습니까?',
+        reply = QMessageBox.question(self, 'Message', 'Would you like to return to the login screen??',
                                      QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
         if reply == QMessageBox.Yes:
             self.go_back()
         else:
             pass
+        
+    def go_back(self):
+        self.from_class = WindowClass()
+        self.from_class.show()
+        self.close()
     
 # -------------------------------------------------------------------------------------------------------------------------------------------
 
-# Sign up Class
 class Sign_up_Window(QDialog, sign_up_class):
     def __init__(self):
         super().__init__()
@@ -349,7 +336,6 @@ class Sign_up_Window(QDialog, sign_up_class):
         self.Sign.clicked.connect(self.sign_up)
         self.Check.clicked.connect(self.Check_id)
         
-    # ID 중복 체크
     def Check_id(self):
         user_id = self.Idld.text()
         
@@ -358,13 +344,12 @@ class Sign_up_Window(QDialog, sign_up_class):
         
         if response.status_code == 401:
             
-            QMessageBox.information(self, 'ID 중복 확인', '해당 ID는 이미 등록되어있습니다.', QMessageBox.Ok)
+            QMessageBox.information(self, 'ID double check', 'This ID is already registered.', QMessageBox.Ok)
             return
         
         else:
-            QMessageBox.information(self, 'ID 확인', '사용 가능한 ID입니다.', QMessageBox.Ok)
+            QMessageBox.information(self, 'ID Check', 'This ID is Available ID.', QMessageBox.Ok)
     
-    # 회원 가입
     def sign_up(self):
         
         user_birthday = self.Birthdayld.text()
@@ -374,24 +359,24 @@ class Sign_up_Window(QDialog, sign_up_class):
         check_password = self.Checkld.text()
         
         if user_password != check_password:
-            QMessageBox.warning(self, "비밀번호 확인", "비밀번호가 일치하지 않습니다.", QMessageBox.Ok)
+            QMessageBox.warning(self, "Password Check", "Passwords do not match.", QMessageBox.Ok)
             return 
 
         if len(user_birthday) > 7:
-            QMessageBox.warning(self, '생년월일 오류', '생년월일은 6자로 입력해주세요.', QMessageBox.Ok)
+            QMessageBox.warning(self, 'Date of birth error', 'Please enter 6 characters for your date of birth.', QMessageBox.Ok)
             return
         
                 
         if len(user_name) > 5:
-            QMessageBox.warning(self, "이름 오류", "이름은 4자 이하로 입력해주세요.", QMessageBox.Ok)
+            QMessageBox.warning(self, "Name error", "Please enter a name of 4 characters or less.", QMessageBox.Ok)
             return
 
         if len(user_id) > 10:
-            QMessageBox.warning(self, "ID 오류", "ID는 10자 이하로 입력해주세요.", QMessageBox.Ok)
+            QMessageBox.warning(self, "ID error", "Please enter ID with 10 characters or less.", QMessageBox.Ok)
             return
 
         if len(user_password) < 8 or len(user_password) > 20:
-            QMessageBox.warning(self, "비밀번호 오류", "비밀번호는 8자 이상 20자 이하로 입력해주세요.", QMessageBox.Ok)
+            QMessageBox.warning(self, "Password error", "Please enter your password between 8 and 20 characters.", QMessageBox.Ok)
             return
             
             
@@ -400,18 +385,16 @@ class Sign_up_Window(QDialog, sign_up_class):
         
         
         if response.status_code == 201:
-            QMessageBox.information(self, '회원가입 성공', '회원가입에 성공하셨습니다.', QMessageBox.Ok)
+            QMessageBox.information(self, 'registration successful', 'You have successfully registered.', QMessageBox.Ok)
             self.accept()
 
         elif response.status_code == 404:
-            QMessageBox.information(self, '회원가입 실패', '서버에 연결 할 수 없습니다.', QMessageBox)
+            QMessageBox.information(self, 'registration fail', 'Unable to connect to server.', QMessageBox)
         
         else:
-            QMessageBox.warning(self, '회원가입 실패', '회원가입에 실패하였습니다.', QMessageBox.Ok)
+            QMessageBox.warning(self, 'registration fail', 'registration failed.', QMessageBox.Ok)
         print(response)
         
-        
-# Main 화면 Class   
 class WindowClass(QMainWindow, from_class) :
     def __init__(self):
         super().__init__()
@@ -422,7 +405,6 @@ class WindowClass(QMainWindow, from_class) :
         self.Signupbtn.clicked.connect(self.open_sign_window)
         self.Loginbtn.clicked.connect(self.Login_play)
         
-    # 로그인
     def Login_play(self):
         user_id = self.Idedit.text()
         user_password = self.Passwordedit.text()
@@ -432,24 +414,22 @@ class WindowClass(QMainWindow, from_class) :
         
         
         if response.status_code == 201:
-            QMessageBox.information(self, '로그인 성공', '로그인에 성공하셨습니다.', QMessageBox.Ok)
+            QMessageBox.information(self, 'Login successful', 'You have successfully logged in.', QMessageBox.Ok)
             self.open_total_gui_window()
             
         elif response.status_code == 404:
-            QMessageBox.information(self, '로그인 실패', '서버에 연결 할 수 없습니다.', QMessageBox)
+            QMessageBox.information(self, 'Login failed', 'Unable to connect to server.', QMessageBox)
         
         else:
-            QMessageBox.warning(self, '로그인 실패', '로그인에 실패하였습니다.', QMessageBox.Ok)
+            QMessageBox.warning(self, 'Login failed', 'Login failed.', QMessageBox.Ok)
         print(response)
         
-    # 회원가입 창 전환
     def open_sign_window(self):
         self.new_window = Sign_up_Window()
         self.close()
         self.new_window.exec_()
         self.show()
 
-    # Total gui 화면 창 전환 (로그인 완료)
     def open_total_gui_window(self):
         self.total_gui_window = Total_gui_Window()
         self.total_gui_window.show()
